c44de5b81fffad48172d8aafa6d3154bdc061c70
Bachitter Chahal <officialbachitterchahal@gmail.com>
2021-10-24 01:17:56 -0700

    responsive top nav

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var x = document.getElementById('nav__menu');
+  if (x.style.right === "100%") {
+    x.style.right = "0";
+  } else {
+    x.style.right = "100%"
+  }
+
+}
