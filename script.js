84e9f233185c709f53b9c0cc2d5c9c865b570805
Bachitter Chahal <officialbachitterchahal@gmail.com>
2021-11-01 03:36:19 -0700

    added contact and footer section

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function myFunction() {
   }
 
 }
+
+mybutton = document.getElementById("btop");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
